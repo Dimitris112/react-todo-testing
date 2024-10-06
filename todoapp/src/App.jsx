00e403b24a3354b70087ca16e8428dc68ef271ab
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-10-06 22:40:39 +0300

    Add InlineComponent and inline styling

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -1,9 +1,11 @@
+import InlineComponent from "./Components/InlineComponent";
 import Todo from "./Components/Todo";
 
 function App() {
   return (
     <div className="App">
-      <Todo />
+      {/* <Todo /> */}
+      <InlineComponent />
     </div>
   );
 }
